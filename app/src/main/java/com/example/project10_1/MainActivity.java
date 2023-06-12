f9667043037343e644d5ffc6f426e70809e34ff9
JUNO SHIN <tlswnsgh0624@naver.com>
2023-06-12 18:49:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/project10_1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.project10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnNewActivity = findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                                        SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/project10_1/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.project10_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedinstancestate) {
+        super.onCreate(savedinstancestate);
+        setContentView(R.layout.second);
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
